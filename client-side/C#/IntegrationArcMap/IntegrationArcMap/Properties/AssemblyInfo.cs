c6779beaf0681ae0d197814f9e0609df0e3d7677
Harm73 <harmbruinsma@home.nl>
2014-03-26 11:40:55 +0100

    Updated AddIn file

--- client-side/C#/IntegrationArcMap/IntegrationArcMap/Properties/AssemblyInfo.cs
+++ client-side/C#/IntegrationArcMap/IntegrationArcMap/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("601756f1-d4d0-4279-abad-47338704585c")]
 
-[assembly: AssemblyVersion("1.1.0.4080")]
-[assembly: AssemblyFileVersion("1.1.0.4080")]
+[assembly: AssemblyVersion("1.1.1.4088")]
+[assembly: AssemblyFileVersion("1.1.1.4088")]
